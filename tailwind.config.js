7547b98aa0a59d3389151c2579f6acb92405d34c
Lucas <bokled602@gmail.com>
2023-05-08 12:31:27 -0300

    Mudança nas Cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}'
@@ -6,12 +8,43 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        fontBlueColor: '#042C71'
-      },
-      fontFamily: {
-        lexend: "font-family: 'Lexend', sans-serif;"
+        fontRegularColor: '#042C71'
       }
+    },
+    colors: {
+      orange: {
+        100: '#FFBC95',
+        200: '#F36B1C',
+        300: '#FF5E00'
+      },
+      notification: {
+        warning: '#F06E67',
+        changes: '#F7C374',
+        progress: '#74BAFA',
+        completed: '#80D465'
+      }
+    },
+    fontFamily: {
+      lexend: "font-family: 'Lexend', sans-serif;"
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.text-regular': {
+          color: '#042C71',
+          fontWeight: '400',
+          lineHeight: '22px',
+          fontSize: '18px'
+        },
+        '.text-title': {
+          color: '#042C71',
+          fontWeight: '600',
+          fontSize: '24px',
+          lineHeight: '30px'
+        }
+      })
+    })
+
+  ]
 }
