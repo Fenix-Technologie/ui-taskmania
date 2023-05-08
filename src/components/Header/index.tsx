c7f21ae5f75e009e6e245f3e1a1202f24082031c
RicardoSobral7 <riki.sobral@gmail.com>
2023-05-08 14:36:37 -0300

    fix text colors, font and login screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../../../public/Logo";
 
 export function Header() {
     return (
-        <div className="w-screen h-28 flex flex-row items-center justify-center bg-white">
+        <div className="w-full h-28 flex flex-row items-center justify-center bg-white">
             <div className="w-full h-full flex flex-col items-center justify-center">
                 <Logo />
                 {/* O logo é em SVG */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,16 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Header } from "@/components/Header";
 import { LogoWhite } from "../../public/LogoWhite";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className="w-scren h-screen flex flex-col items-center justify-center bg-slate-300">
-            <div className="w-full h-full flex flex-col items-center justify-center">
-                <div className='w-80 h-96 flex flex-col items-center justify-center rounded-xl bg-gradient-to-l from-[#FFBC95] to-[#FF5E00] gap-y-5'>
-                    <LogoWhite/>
+        <main className="w-full h-[calc(100vh-7rem)] flex flex-col items-center justify-center bg-slate-300">
+            <section className="w-full h-full flex flex-col items-center justify-center">
+                <div className='w-80 h-96 flex flex-col items-center justify-center rounded-xl bg-gradient-to-l from-orange-100 to-orange-300 gap-y-5'>
+                    <LogoWhite />
                     <div className="w-full flex flex-col items-center justify-center">
                         <div className='w-full flex flex-col items-start justify-start ml-28'>
                             <label htmlFor="" className="text-fontBlueColor font-semibold font-lexend text-base">E-mail</label>
@@ -21,8 +22,6 @@ export default function Home() {
                             <input type="text" className='rounded-lg p-2 border-[#B0B0B0] border-[1px] drop-shadow-md' />
                         </div>
                     </div>
-
-                    
                     <div className="w-full flex flex-col items-center justify-center">
                         <div className="w-full flex flex-col items-start justify-start ml-28">
                             <label htmlFor="" className="text-fontBlueColor font-semibold font-lexend text-base">Password</label>
@@ -32,9 +31,14 @@ export default function Home() {
                         </div>
                     </div>
                     <button className='rounded-lg py-2 px-10 text-white font-bold font-lexend bg-gradient-to-r from-[#3060B2] via-[#042C71] to-[#3060B2]'>Sing in</button>
-                    <span className='text-white font-lexed'>Don’t have an account? Sign up now!</span>
+                    <span className='text-white font-lexed' >Don’t have an account?
+                    {' '}
+                        <Link href="#" className="text-regular text-white hover:underline"> 
+                            Sign up now!
+                        </Link>
+                    </span>
                 </div>
-            </div>
+            </section>
         </main>
     );
 }
